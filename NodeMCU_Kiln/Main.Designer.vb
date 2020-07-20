7e12bc2091c11627330b84a38be3a5e1cb3d48e9
Dale Thomas <dalethomas81@outlook.com>
2020-07-20 13:16:39 -0400

    add easymodbus

--- NodeMCU_Kiln/Main.Designer.vb
+++ NodeMCU_Kiln/Main.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,38 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(243, 134)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Main"
+        Me.Text = "Main"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
 End Class
---
+++ NodeMCU_Kiln/Main.vb
@@ -0,0 +1,24 @@
+﻿Public Class Main
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim mb As New EasyModbus.ModbusClient("COM26", 502)
+        mb.Baudrate = 115200
+        mb.Parity = IO.Ports.Parity.None
+        mb.StopBits = IO.Ports.StopBits.One
+
+        mb.Connect()
+
+        If mb.Connected Then
+            Dim qty As Int16 = 2
+            Dim TempInt32(qty) As Int32
+            TempInt32 = mb.ReadInputRegisters(0, qty)
+
+            Dim TempFloat = EasyModbus.ModbusClient.ConvertRegistersToDouble(TempInt32)
+            Label1.Text = TempFloat.ToString
+        End If
+
+        mb.Disconnect()
+    End Sub
+
+End Class
